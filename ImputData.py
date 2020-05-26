ef1ed21b1cc4adc4c1a21fcb73babff9356d132a
etsuura <etsuura@gmail.com>
2020-05-26 17:25:30 +0900

    add getInputData add standardization

--- ImputData.py
+++ ImputData.py
@@ -13,11 +13,34 @@ def read_audio(FilePath):
 
     return fo, mcep
 
-#Todo 正規化
-# def getInputaData(fo, mcep):
-#     array = np.zeros(40,5)
-#
-#     return array
+def standardization(x, axis=None, ddof=0):
+    x_mean = x.mean(axis=axis, keepdims=True)
+    x_std = x.std(axis=axis, keepdims=True, ddof=ddof)
+    return (x - x_mean) / x_std
+
+def check_silent(val):
+    if val != 0:
+        status = 1
+    else:
+        status = 0
+    return status
+
+def getInputaData(fo, mcep):
+    mcep_array = np.zeros((5, 39))
+    fo_array = np.zeros(5)
+
+    mcep_array[2, :] = mcep[0, 1:40]
+    mcep_array[3, :] = mcep[1, 1:40]
+    mcep_array[4, :] = mcep[2, 1:40]
+    mcep_array_std = standardization(mcep_array)
+    mcep_array_std_flat = mcep_array_std.flatten()
+
+    # check silent
+    fo_array[2] = check_silent(fo[0])
+    fo_array[3] = check_silent(fo[1])
+    fo_array[4] = check_silent(fo[2])
+
+    return mcep_array_std_flat, fo_array
 
 # def plotParam(fo, mcep):
 #     pf.plot_1figure(fo)
@@ -28,4 +51,5 @@ if __name__ == '__main__':
     FilePath = "./Data/jvs001/VOICEACTRESS100_001.wav"
 
     fo, mcep = read_audio(FilePath)
+    getInputaData(fo, mcep)
     # plotParam(fo, mcep)
